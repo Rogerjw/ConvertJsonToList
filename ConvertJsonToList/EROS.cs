7a11f8a082133aac33ff8544bf7fea0342b225bc
rogerm@techaffinity.com <rogerm@techaffinity.com>
2023-03-16 11:01:06 +0200

    Add project files.

---
+++ ConvertJsonToList/EROS.cs
@@ -0,0 +1,42 @@
+﻿public class Rootobject
+{
+    public ERO[] EROS { get; set; }
+}
+
+public class ERO
+{
+    public string moduleName { get; set; }
+    public string URL { get; set; }
+    public string Screename { get; set; }
+    public Field[] Fields { get; set; }
+    public Savedata[] SaveData { get; set; }
+    public Alreadyexistsdata[] AlreadyExistsData { get; set; }
+    public Searchdata[] SearchData { get; set; }
+}
+
+public class Field
+{
+    public string fieldName { get; set; }
+    public string locatorId { get; set; }
+    public bool mandatory { get; set; }
+}
+
+public class Savedata
+{
+    public string fieldname { get; set; }
+    public string fieldvalue { get; set; }
+}
+
+public class Alreadyexistsdata
+{
+    public string fieldname { get; set; }
+    public string fieldvalue { get; set; }
+}
+
+public class Searchdata
+{
+    public string fieldname { get; set; }
+    public string fieldvalue { get; set; }
+}
+
+
---
+++ ConvertJsonToList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace ConvertJsonToList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = System.IO.Directory.GetCurrentDirectory();
+            var actualPath = path.Substring(0, path.LastIndexOf("bin", StringComparison.Ordinal));
+            var projectPath = new Uri(actualPath).LocalPath;
+            var file = projectPath + "\\json1.json";
+
+            var eros = ReadJsonFile(file).EROS;
+            foreach (var ero in eros)
+            {
+                Console.WriteLine($"{ero.moduleName} {ero.URL}, {ero.Screename}");
+            }
+        }
+        public static Rootobject ReadJsonFile(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<Rootobject>(json);
+        }
+
+    }
+}
